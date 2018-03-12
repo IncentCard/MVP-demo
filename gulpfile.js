8a5b1039102112fa8cd1434173cab10bef92f47f
kevin-incentcard <kevin@incentcard.com>
2018-03-12 16:21:14 -0700

    Converted old demo into a runnable web server using node.js and express

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const HTML_FILES = ['src/*.html', 'src/**/*.html'];
+
+// pull in the project TypeScript config
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+  .pipe(tsProject());
+  return tsResult.js.pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function() {
+  return gulp.src(HTML_FILES)
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('assets', function() {
+  return gulp.src(JSON_FILES)
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['watch', 'assets']);
